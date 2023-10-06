a38dab0cfbafad8e8691fcba3e753b0ab025d176
kouyate karim <kouyatekarim02@gmail.com>
2023-10-06 10:23:11 +0000

    first

---
+++ front/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Aside from './components/Aside';
+import ChatBox from './components/ChatBox';
+
+function App() {
+  return (
+    <div className="app">
+      <Aside/>
+      <ChatBox/>
+    </div>
+  );
+}
+
+export default App;
---
+++ front/src/components/Aside.jsx
@@ -0,0 +1,20 @@
+
+import React from 'react'
+
+function Aside() {
+  return (
+    <div className="aside">
+        <div className="header">
+            <div className="new-chat">
+              
+            </div>
+        </div>
+
+        <div className="footer">
+
+        </div>
+    </div>
+  )
+}
+
+export default Aside
